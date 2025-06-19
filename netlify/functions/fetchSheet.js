be8ca168b4ccec111435071d9a1dbf822c163f89
Valdran <charles.lucius.belmont@gmail.com>
2025-06-18 22:02:44 -0400

    fetchSheet.js

---
+++ netlify/functions/fetchSheet.js
@@ -0,0 +1,13 @@
+export async function handler(event, context) {
+  const res = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSsV-TRNygrIsnsAZNB-nO5NEOand8W-ywRl1v4YYGLMOoiEcbknkVWsU_aI0VYzPkPywdHdTM-0enj/pub?output=csv");
+  const text = await res.text();
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/csv',
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: text
+  };
+}
